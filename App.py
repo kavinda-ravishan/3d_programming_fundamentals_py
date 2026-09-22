5156fafec84e6e17892ae32a191b41dde03e0926
kavinda-ravishan <kvnravishan@outlook.com>
2026-09-22 15:49:42 +0530

    Transformations / Scaling / Translation

--- App.py
+++ App.py
@@ -1,36 +1,75 @@
+from math import cos, sin
+from copy import deepcopy
 from Utils import Vec2, Color, Game, Scene
 
-class SimpleScene(Scene):
+class Star:
+    @staticmethod
+    def Make(outer_radius: float, inner_radius: float, n_flares: int):
+        star: list[Vec2] = []
+        d_theta = (2.0 * 3.14159) / (n_flares * 2)
+
+        for i in range(n_flares * 2):
+            rad = outer_radius if (i % 2 == 0) else inner_radius
+            star.append(
+                Vec2(rad * cos(i * d_theta),  rad * sin(i * d_theta))
+            )
+
+        return star
+
+class Entity:
+    def __init__(self, model: list[Vec2]):
+        self.model = model
+        self.pos = Vec2(0, 0)
+        self.scale = 1.0
+
+    def TranslateBy(self, offset: Vec2):
+        self.pos += offset
+
+    def ScaleBy(self, by: float):
+        self.scale *= by
+
+    def GetPolyLine(self) -> list[Vec2]:
+        poly = deepcopy(self.model)
+        for i in range(len(poly)):
+            poly[i] *= self.scale
+            poly[i] += self.pos
+
+        return poly
+
+class PolylinesScene(Scene):
     def __init__(self):
         super().__init__()
-        self.p0 = Vec2()
-        self.p1 = Vec2()
-        self.m_lb = False
-        self.x_max = 0
-        self.y_max = 0
+
+        self.frame_width: int | None = None
+        self.frame_height: int | None = None
+        self.entity = Entity(Star.Make(150.0, 75.0, 8))
         
-    def GraphicsSetupComplete(self): ...
+    def CompsSetupComplete(self):
+        self.frame_width = self.ct.GetFrameWidth()
+        self.frame_height = self.ct.GetFrameHeight()
 
     def Update(self, key: str, mouse_stat: tuple[tuple[int, int], bool, bool], dt: float):
-        m_x = mouse_stat[0][0]
-        m_y = mouse_stat[0][1]
-        self.m_lb = mouse_stat[1]
+        # m_x = mouse_stat[0][0]
+        # m_y = mouse_stat[0][1]
+        # m_lb = mouse_stat[1]
+        # m_rb = mouse_stat[2]
 
-        if(not self.m_lb):
-            self.p0.x = m_x
-            self.p0.y = m_y
+        speed = 20.0
+        if 'w' == key: self.entity.TranslateBy(Vec2(0.0, speed))
+        elif 's' == key: self.entity.TranslateBy(Vec2(0.0, -speed))
+        elif 'd' == key: self.entity.TranslateBy(Vec2(speed, 0.0))
+        elif 'a' == key: self.entity.TranslateBy(Vec2(-speed, 0.0))
 
-        self.p1.x = m_x
-        self.p1.y = m_y
+        elif 'q' == key: self.entity.ScaleBy(0.95)
+        elif 'e' == key: self.entity.ScaleBy(1.05)
     
     def Draw(self):
-        if self.m_lb:
-            self.gfx.DrawLineVec(self.p0, self.p1, Color(255, 255, 255))
+        self.ct.DrawClosePolyline(self.entity.GetPolyLine(), Color.Red)
 
 if '__main__' == __name__:
 
     try:
-        game = Game(1280, 720, SimpleScene())
+        game = Game(1280, 720, 30, [PolylinesScene()])
         game.Go()
     except Exception as ex:
         print(f'Exeption : {ex}')
--- Utils.py
+++ Utils.py
@@ -13,6 +13,12 @@ class Vec2:
         else:
             super().__setattr__(name, value)
 
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, scalar: float):
+        return Vec2(self.x * scalar, self.y * scalar)
+
 class Color:
     def __init__(self, r: int = 0, g: int = 0, b: int = 0):
         self.r = int(r)
@@ -24,6 +30,17 @@ class Color:
             super().__setattr__(name, int(value))
         else:
             super().__setattr__(name, value)
+
+Color.White = Color(255, 255, 255)
+Color.Black = Color(0, 0, 0)
+Color.Gray = Color(0x80, 0x80, 0x80)
+Color.LightGray = Color(0xD3, 0xD3, 0xD3)
+Color.Red = Color(255, 0, 0)
+Color.Green = Color(0, 255, 0)
+Color.Blue = Color(0, 0, 255)
+Color.Yellow = Color(255, 255, 0)
+Color.Cyan = Color(0, 255, 255)
+Color.Magenta = Color(255, 0, 255)
 
 class Surface:
     def __init__(self, width: int, height: int):
@@ -38,7 +55,8 @@ class Surface:
         self.canvas = np.zeros_like(self.canvas)
 
     def PutPixel(self, x: int, y: int, color: Color):
-        self.canvas[int(y),int(x)] = (color.b, color.g, color.r)
+        if(x >=0 and y >= 0 and x < self.width and y < self.height):
+            self.canvas[int(y),int(x)] = (color.b, color.g, color.r)
 
     def GetFrameWidth(self): return self.width
     def GetFrameHeight(self): return self.height
@@ -136,19 +154,6 @@ class Graphics:
 
     # Bresenham's Line Algorithm
     def DrawLineXY(self, x1: float, y1: float, x2: float, y2: float, color: Color):
-        x_max = self.GetFrameWidth() - 1
-        y_max = self.GetFrameHeight() - 1
-
-        x1 = max(x1, 0)
-        x1 = min(x1, x_max)
-        x2 = max(x2, 0)
-        x2 = min(x2, x_max)
-
-        y1 = max(y1, 0)
-        y1 = min(y1, y_max)
-        y2 = max(y2, 0)
-        y2 = min(y2, y_max)
-
         dx = x2 - x1
         dy = y2 - y1
 
@@ -160,18 +165,18 @@ class Graphics:
                 y1, y2 = y2, y1
 
             m = dx / dy
-            lastIntY = 0
+            last_int_y = 0
             y = y1
             x = x1
             while y < y2:
 
-                lastIntY = int(y)
-                self.PutPixel(int(x), lastIntY, color)
+                last_int_y = int(y)
+                self.PutPixel(int(x), last_int_y, color)
 
                 x += m
                 y += 1
                 
-            if int(y2) > lastIntY:
+            if int(y2) > last_int_y:
                 self.PutPixel(int(x2), int(y2), color)
         else:
             if dx < 0.0:
@@ -179,33 +184,35 @@ class Graphics:
                 y1, y2 = y2, y1
 
             m = dy / dx
-            lastIntX = 0
+            last_int_x = 0
             x = x1
             y = y1
             while x < x2:
 
-                lastIntX = int(x);
-                self.PutPixel(lastIntX, int(y), color);
+                last_int_x = int(x);
+                self.PutPixel(last_int_x, int(y), color);
 
                 y += m
                 x += 1
             
-            if int(x2) > lastIntX:
+            if int(x2) > last_int_x:
                 self.PutPixel(int(x2), int(y2), color);
 
-    def DrawLineSimpleVec(self, p0: Vec2, p1: Vec2, color: Color):
-        x_max = self.GetFrameWidth() - 1
-        y_max = self.GetFrameHeight() - 1
+    def DrawLineSimpleVec(self, p0: Vec2, p1: Vec2, color: Color, clip: bool = False):
 
-        p0.x = max(p0.x, 0)
-        p0.x = min(p0.x, x_max)
-        p1.x = max(p1.x, 0)
-        p1.x = min(p1.x, x_max)
+        if clip:
+            x_max = self.GetFrameWidth() - 1
+            y_max = self.GetFrameHeight() - 1
 
-        p0.y = max(p0.y, 0)
-        p0.y = min(p0.y, y_max)
-        p1.y = max(p1.y, 0)
-        p1.y = min(p1.y, y_max)
+            p0.x = max(p0.x, 0)
+            p0.x = min(p0.x, x_max)
+            p1.x = max(p1.x, 0)
+            p1.x = min(p1.x, x_max)
+
+            p0.y = max(p0.y, 0)
+            p0.y = min(p0.y, y_max)
+            p1.y = max(p1.y, 0)
+            p1.y = min(p1.y, y_max)
 
         m = 0.0
         if p0.x != p1.x:
@@ -232,16 +239,40 @@ class Graphics:
                 x = (m * y) + b
                 self.PutPixel(x, y, color)
 
+    def DrawClosePolyline(self, verts: list[Vec2],  color: Color):
+
+        for i in range(0, len(verts) - 1):
+            p0 = verts[i]
+            p1 = verts[i+1]
+
+            self.DrawLineVec(p0, p1, color)
+
+        self.DrawLineVec(verts[0], verts[-1], color)
+
+class CoordinateTransformer:
+    def __init__(self, graphics: Graphics):
+        self.gfx = graphics
+
+    def GetFrameWidth(self): return self.gfx.GetFrameWidth()
+    def GetFrameHeight(self): return self.gfx.GetFrameHeight()
+
+    def DrawClosePolyline(self, verts: list[Vec2],  color: Color):
+        offset = Vec2(self.gfx.GetFrameWidth() / 2, self.gfx.GetFrameHeight() / 2)
+        for i in range(len(verts)):
+            verts[i].y *= -1
+            verts[i] += offset
+        self.gfx.DrawClosePolyline(verts, color)
+
 class Scene(ABC):
     def __init__(self):
-        self.gfx: Graphics | None = None
+        self.ct: CoordinateTransformer | None = None
 
     @abstractmethod
-    def GraphicsSetupComplete(self): pass
+    def CompsSetupComplete(self): pass
          
-    def SetGraphics(self, graphics: Graphics):
-        self.gfx = graphics
-        self.GraphicsSetupComplete()
+    def SetComps(self, coordinate_transformer: CoordinateTransformer):
+        self.ct = coordinate_transformer
+        self.CompsSetupComplete()
 
     @abstractmethod
     def Update(self, key: str, mouse_stat: tuple[tuple[int, int], bool, bool], dt: float): pass
@@ -250,39 +281,44 @@ class Scene(ABC):
     def Draw(self): pass
 
 class Game:
-    def __init__(self, frame_width, frame_height, scene: Scene):
+    def __init__(self, frame_width, frame_height, fps, scenes : list[Scene]):
         self.main_loop_active = True
         window_name = "Canvas"
-        fps = 60.0
         self.dt = 1.0 / fps
         time_per_frame_ms = self.dt * 1000
-        self.graphics = Graphics(window_name, frame_width, frame_height, int(time_per_frame_ms))
-        self.scene = scene
-        self.scene.SetGraphics(self.graphics)
+        self.gfx = Graphics(window_name, frame_width, frame_height, int(time_per_frame_ms))
+        self.ct = CoordinateTransformer(self.gfx)
+
+        self.c_scene_id = 0
+        self.scenes = scenes
+        for scene in scenes:
+            scene.SetComps(self.ct)
 
     def UpdateModel(self):
-        key = self.graphics.GetKey()
-        mouse_stat = self.graphics.GetMouseState()
+        key = self.gfx.GetKey()
+        mouse_stat = self.gfx.GetMouseState()
 
-        self.scene.Update(key, mouse_stat, self.dt)
+        self.scenes[self.c_scene_id].Update(key, mouse_stat, self.dt)
         self.ManageInputs(key)
 
     def ComposeFrame(self):
-        self.scene.Draw()
+        self.scenes[self.c_scene_id].Draw()
 
     def ManageInputs(self, key: str):
-            if(key == 'esc'):
+            if key == 'esc':
                 self.main_loop_active = False
+            elif key == 'tab':
+                self.c_scene_id = (self.c_scene_id + 1)%len(self.scenes)
 
     def Go(self):
         print("Game loop started")
         while self.main_loop_active:
-            self.graphics.BeginFrame()
+            self.gfx.BeginFrame()
 
             self.ComposeFrame()
 
-            self.graphics.EndFrame()
-            self.graphics.Wait()
+            self.gfx.EndFrame()
+            self.gfx.Wait()
 
             self.UpdateModel()
 
